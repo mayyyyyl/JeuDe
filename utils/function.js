da9c90021bf06f368cf458e9c0fc5ca8998857f7
Maylis Le Borgne <maylislb@hotmail.fr>
2022-01-30 22:07:37 +0100

    lancer dé, stocker score

--- utils/function.js
+++ utils/function.js
@@ -1,25 +1,35 @@
-
 const hold = document.getElementById("hold");
 const roledice = document.getElementById("roledice");
 const play = document.getElementById("play");
 const newgame = document.getElementById("newgame");
 
+/* Classe joueur*/
 class player {
     constructor(pseudo) {
         this.pseudo = pseudo
     }
 }
 
-/* création des joueurs */
+/* Création des joueurs */
 let player1 = new player(this.pseudo);
 let player2 = new player(this.pseudo);
 
+
+/* Ecoute des boutons */
 play.addEventListener('click', () => {
     getName();
 });
 
 newgame.addEventListener('click', () => {
     startNewGame();
+});
+
+roledice.addEventListener('click', () => {
+    rollDice();
+});
+
+hold.addEventListener('click', () => {
+    holdscore();
 });
 
 /* Choisir son nom */
@@ -48,33 +58,57 @@ function getName() {
     play.setAttribute("disabled", "disabled");
 }
 
+/* Commencer une nouvelle partie*/
 function startNewGame() {
     play.removeAttribute("disabled");
     game.setAttribute("hidden", "hidden");
 }
 
+let score = [0, 0];
+let scoreglobal = [0, 0];
+let players = [player1, player2];
+let turn = 0;
 
-// function myStopFunction() {
-//     clearInterval(myInterval);
-// }
 
-// function toggleStatus() {
-//     statut = !statut
-//     if (statut == true) {
-//         img.src = "static/img/stop.png";
+/* Lancer de dé*/
+function rollDice() {
 
-//         if (beginTimestamp == null) {
-//             beginTimestamp = Date.now()
-//         }
-//         endTimestamp = null;
-//         myInterval = setInterval(myTimer, 1000);
-//         chantierSelect.setAttribute("disabled", "disabled");
-//         rangestart();
-//     }
-//     else {
-//         img.src = "static/img/play.png";
-//         myStopFunction();
-//     }
-// }
+    let currentPlayer = players[turn];
 
-// img.addEventListener("click", toggleStatus);
+    diceNumber = Math.floor(Math.random() * 6) + 1;
+    console.log(diceNumber);
+
+    if (diceNumber == 1) {
+
+        score[turn] = 0;
+
+        turn = turn + 1;
+        if (turn == players.length) {
+            turn = 0;
+        }
+    }
+    else {
+        score[turn] += diceNumber;
+    }
+
+    return document.getElementById("diceNumber").innerHTML = diceNumber,
+        document.getElementById("round1").innerHTML = "Courant : " + score[0],
+        document.getElementById("round2").innerHTML = "Courant : " + score[1];
+}
+
+function holdscore() {
+
+    scoreglobal[turn] += score[turn];
+
+    if (scoreglobal[turn] >= 100) {
+        console.log("Vous avez gagné !!!!!!!");
+    }
+
+    score[turn] = 0;
+    turn = turn + 1;
+    if (turn == players.length) {
+        turn = 0;
+    }
+    return document.getElementById("global1").innerHTML = "Global : " + scoreglobal[0],
+        document.getElementById("global2").innerHTML = "Global : " + scoreglobal[1];
+}
